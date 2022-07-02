9cb3dcb6d2cd227d3ef8da58a6459048f14ae274
Danyal <danialtariq43@gmail.com>
2022-07-03 00:36:12 +0500

    Player Movement and Environment Setup
    # Player movement added
    # Player Animation added using spine example asset
    # NPC Skin Selection Added , later to be used in character customization as well.
    # Basic Shop Interior Setup using tilemaps

---
+++ Assets/Scripts/Core/NPC/NPCSpineSkinSetup.cs
@@ -0,0 +1,35 @@
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+	public class NPCSpineSkinSetup : MonoBehaviour
+	{
+
+	public SkeletonDataAsset skeletonDataAsset;
+    public SkeletonAnimation skeletonAnim;
+
+    [SpineSkin] public string baseSkin = "skin-base";
+    [SpineSkin] public string Shirt;
+    [SpineSkin] public string Pant;
+    [SpineSkin] public string Hair;
+    [SpineSkin] public string Eyes;
+
+    private void Start()
+    {
+        print(Shirt);
+        var skeletonData = skeletonAnim.skeleton.Data;
+        Skin addedSkins = new Skin("multiSkin");
+
+        addedSkins.AddSkin(skeletonData.FindSkin(baseSkin));
+        addedSkins.AddSkin(skeletonData.FindSkin(Shirt));
+        addedSkins.AddSkin(skeletonData.FindSkin(Pant));
+        addedSkins.AddSkin(skeletonData.FindSkin(Hair));
+        addedSkins.AddSkin(skeletonData.FindSkin(Eyes));
+       
+        skeletonAnim.skeleton.SetSkin(addedSkins);
+        skeletonAnim.skeleton.SetSlotsToSetupPose();
+    }
+
+}
